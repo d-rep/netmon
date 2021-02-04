420751deb86d0cdbd593f9c42f3c226d06a0118a
Drew Repasky <drew@repasky.org>
2021-02-03 21:54:27 -0500

    adding go fiber templates which may not be kept due to the  dependency tree

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/gofiber/fiber/v2 v2.4.1
+	github.com/gofiber/template v1.6.6
 	github.com/jmoiron/sqlx v1.3.1
 	github.com/mattn/go-sqlite3 v1.14.6
 )
--- web/web.go
+++ web/web.go
@@ -3,15 +3,29 @@ package web
 import (
 	"log"
 
+	"github.com/gofiber/template/html"
+
 	"github.com/gofiber/fiber/v2"
 	"gitlab.com/drep/netmon/storage"
 )
 
 func Serve(port string, db *storage.Storage) error {
-	app := fiber.New()
+	engine := html.New("./views", ".html")
+	engine.Reload(true)
+	engine.Debug(true)
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("hello world!")
+		calls, err := db.GetRecentCalls(10)
+		if err != nil {
+			log.Println(err)
+		}
+		return c.Render("index", fiber.Map{
+			"Title": "Network Monitor",
+			"calls": calls,
+		})
 	})
 
 	app.Get("/status", func(c *fiber.Ctx) error {
