c5f15ce75e2a910c2c5ad6603b612db837b83afc
Drew Repasky <drew@repasky.org>
2022-08-09 08:51:26 -0400

    rip out Fiber and use stdlib http instead, and embed the HTML template directly in the binary

--- go.mod
+++ go.mod
@@ -3,17 +3,6 @@ module github.com/d-rep/netmon
 go 1.19
 
 require (
-	github.com/gofiber/fiber/v2 v2.32.0
-	github.com/gofiber/template v1.6.27
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/mattn/go-sqlite3 v1.14.6
-)
-
-require (
-	github.com/andybalholm/brotli v1.0.4 // indirect
-	github.com/klauspost/compress v1.15.0 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.35.0 // indirect
-	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
 )
--- web/web.go
+++ web/web.go
@@ -1,40 +1,70 @@
 package web
 
 import (
+	_ "embed"
+	"encoding/json"
 	"log"
-
-	"github.com/gofiber/template/html"
+	"net/http"
+	"text/template"
 
 	"github.com/d-rep/netmon/storage"
-	"github.com/gofiber/fiber/v2"
 )
 
+var (
+	//go:embed index.html
+	indexTemplate string
+)
+
+type TemplateData struct {
+	Title string
+	Calls []*storage.Call
+}
+
+func GetIndex(db *storage.Storage) http.HandlerFunc {
+	t, err := template.New("tmpl").Parse(indexTemplate)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		calls, err := db.GetRecentCalls(10)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		templateData := TemplateData{
+			Title: "Network Monitor",
+			Calls: calls,
+		}
+
+		err = t.Execute(w, templateData)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func GetStatus(db *storage.Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		calls, err := db.GetRecentCalls(10)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		b, err := json.Marshal(calls)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Write(b)
+	}
+}
+
 func Serve(port string, db *storage.Storage) error {
-	engine := html.New("./views", ".html")
-	engine.Reload(true)
-	engine.Debug(true)
-	app := fiber.New(fiber.Config{
-		Views: engine,
-	})
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		calls, err := db.GetRecentCalls(10)
-		if err != nil {
-			log.Println(err)
-		}
-		return c.Render("index", fiber.Map{
-			"Title": "Network Monitor",
-			"Calls": calls,
-		})
-	})
-
-	app.Get("/status", func(c *fiber.Ctx) error {
-		calls, err := db.GetRecentCalls(10)
-		if err != nil {
-			log.Println(err)
-		}
-		return c.JSON(calls)
-	})
-
-	return app.Listen("localhost:" + port)
+	http.Handle("/", GetIndex(db))
+	http.Handle("/status", GetStatus(db))
+	return http.ListenAndServe("localhost:"+port, nil)
 }
